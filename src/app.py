066837a6105775f29dec07f48e92700eb6d86fc8
jphafelin <josepablo.hafelin@gmail.com>
2023-03-15 16:15:13 +0000

    Star Wars REST API sin terminar

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,143 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET', 'POST'])
+def user():
+    if request.method == "GET":
+        users = User.query.all()
+        results = [user.serialize() for user in users]
+        response_body = {"message": "ok",
+                        "results": results,
+                        "Total_records": len(results)}
+        return response_body, 200
+    elif request.method == "POST":
+        request_body = request.get_json()
+        user = User(email = request_body['email'],
+                    password = request_body['password'])
+        db.session.add(user)
+        db.session.commit()
+        response_body = {"details": request_body,
+                         "message": "User created"}
+        return response_body, 200
+    else:
+        response_body = {"message": "Error. Method not allowed."}
+        return response_body, 400
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        results = user.serialize()
+        response_body = {"message": "ok",
+                         "total_records": 1,
+                         "results": results}
+        return response_body, 200
+    else:
+        response_body = {"message": "record not found"}
+        return response_body, 200
 
-    return jsonify(response_body), 200
+@app.route('/people', methods=['GET', 'POST'])
+def people():
+    if request.method == "GET":
+        peoples = People.query.all()
+        results = [people.serialize() for people in peoples]
+        response_body = {"message": "ok",
+                        "results": results,
+                        "Total_records": len(results)}
+        return response_body, 200
+    elif request.method == "POST":
+        request_body = request.get_json()
+        peoples = People(email = request_body['email'],
+                         password = request_body['password'])
+        db.session.add(peoples)
+        db.session.commit()
+        response_body = {"details": request_body,
+                         "message": "User created"}
+        return response_body, 200
+    else:
+        response_body = {"message": "Error. Method not allowed."}
+        return response_body, 400
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_people(people_id):
+    peoples = People.query.filter_by(id=people_id).first()
+    if peoples:
+        results = peoples.serialize()
+        response_body = {"message": "ok",
+                         "total_records": 1,
+                         "results": results}
+        return response_body, 200
+    else:
+        response_body = {"message": "record not found"}
+        return response_body, 200
+
+@app.route('/planets', methods=['GET'])
+def planets():
+    content = Planets.query.all()
+    if content:
+        result = [planets.serialize() for planets in content]
+        response_body = {"message": "ok",
+                        "results": result,
+                        "Total_records": len(result)}
+        return response_body, 200
+    else:
+        response_body = {"message": "record not found"}
+        return response_body, 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    content = Planets.query.filter_by(id = planet_id).first()
+    if content:
+        results = content.serialize()
+        response_body = {"message": "Ok",
+                         "records": 1,
+                         "results": results}
+        return response_body, 200
+
+@app.route('/favorites/people', methods=['GET'])
+def get_fav():
+    current_user = 1
+    cuser = User.query.filter_by(id=current_user).first()
+    content = cuser.favorite_people
+    result = [favorite.name for favorite in content]
+    response_body = {"message": "ok",
+                     "results": result}
+    return response_body, 200
+
+@app.route('/favorites/people/<int:people_id>', methods=['POST', 'DELETE'])
+def mod_people_fav(people_id):
+    if request.method == "POST":
+        current_user = 1
+        cuser = User.query.filter_by(id=current_user).first() # Usuario que está haciendo cosas
+        fav_people = People.query.filter_by(id=people_id).first() # Personaje que vamos a añadir
+        if fav_people in cuser.favorite_people:
+            response_body = {"message": "Error. Already exists"}
+            return response_body, 400
+        else:
+            cuser.favorite_people.append(fav_people)
+            db.session.commit()
+            response_body = {"message": "ok",
+                            "added": fav_people.name}
+            return response_body, 200
+    elif request.method == "DELETE":
+        current_user = 1
+        cuser = User.query.filter_by(id=current_user).first() # Usuario que está haciendo cosas
+        fav_people = People.query.filter_by(id=people_id).first() # Personaje que vamos a eliminar
+        if fav_people in cuser.favorite_people:
+            cuser.favorite_people.remove(fav_people)
+            db.session.commit()
+            response_body = {"message": "ok",
+                            "deleted": fav_people.name}
+            return response_body, 200
+        else:
+            response_body = {"message": "Error. Record not found."}
+            return response_body, 404
+    else:
+        response_body = {"message": "Error. Method not allowed."}
+        return response_body, 400
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,69 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+user_planet = db.Table('user_planet',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('planet_id', db.Integer, db.ForeignKey('planets.id')))
+
+user_people = db.Table('user_people',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('people_id', db.Integer, db.ForeignKey('people.id')))
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    # Favorites
+    favorite_planet = db.relationship('Planets', secondary=user_planet)
+    favorite_people = db.relationship('People', secondary=user_people)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    birthyear = db.Column(db.String(50))
+    homeworld = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {
+            "Name": self.name,
+            "ID": self.id,
+            "Height": self.height,
+            "Mass": self.mass,
+            "Birthyear": self.birthyear,
+            "Homeworld": self.homeworld
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    diameter = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(100))
+    terrain = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "Name": self.name,
+            "ID": self.id,
+            "Diameter": self.diameter,
+            "Population": self.population,
+            "Climate": self.climate,
+            "Terrain": self.terrain
         }
